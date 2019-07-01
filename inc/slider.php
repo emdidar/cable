ea4d9dd9cc71d4a64aa8f4dce36486f639d645a1
Didarul Alam <emdidar@gmail.com>
2019-07-01 17:21:14 +0600

    update

---
+++ inc/slider.php
@@ -0,0 +1,17 @@
+<div class="header_bottom">
+        <div class="content">
+        <!-- FlexSlider -->             
+			<section class="slider">
+				  <div class="flexslider">
+					<ul class="slides">
+						<li><img src="images/slider/1.jpg" alt=""/></li>
+						<li><img src="images/slider/2.jpg" alt=""/></li>
+						<li><img src="images/slider/3.jpg" alt=""/></li>
+						<!--<li><img src="images/slider/4.jpg" alt=""/></li>-->
+				    </ul>
+				  </div>
+	      </section>
+        <!-- FlexSlider -->
+	    </div>
+	  <div class="clear"></div>
+ </div>	
